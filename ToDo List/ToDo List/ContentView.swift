b8f3e3b1e7a46c687bf08830260488a219274163
Kamil Uyanik <uyanik@uni-bremen.de>
2024-04-21 00:34:10 +0200

    ToDo List App Initial Commit

---
+++ ToDo List/ToDo List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDo List
+//
+//  Created by Kamil Uyanık on 21.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo List/ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Kamil Uyanık on 21.04.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
