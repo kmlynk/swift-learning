08ee6d92a9c60dc7879de9fa6c02326c782843c8
Kamil Uyanik <uyanik@uni-bremen.de>
2024-04-21 02:52:00 +0200

    Implementing Register View

--- ToDo List/ToDo List/Views/RegisterView.swift
+++ ToDo List/ToDo List/Views/RegisterView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            HeaderView(
+                title: "Register",
+                subtitle: "Start to be Organized",
+                angle: -15,
+                background: .yellow)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("E-Mail Adress", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.none)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                ToDoListButton(
+                    title: "Create an Account",
+                    background: .yellow
+                ) {
+                    // Attempt to Register
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            
+            Spacer()
+        }
     }
 }
 
